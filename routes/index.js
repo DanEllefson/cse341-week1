b0c3915d96d273f701fe55a223a70117a68fad18
Dan Ellefson <41584162+DanEllefson@users.noreply.github.com>
2025-01-28 11:55:48 -0600

    Added Swagger modules and associated code. Working but needs some debug for the PUT request.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@
 
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get(
+  '/api-docs',
+  swaggerUi.setup(swaggerDocument)
+  // #swagger.ignore = true
+);
 
 // Mount a sub-router to handle all routes under /contacts
 router.use('/contacts', require('./contacts'));
